3026c0cb7b7708f35db42f0584c1206295b1ca49
krishna hemke <krishnahemke242@gmail.com>
2023-05-24 12:30:04 +0530

    committed project

---
+++ Book.java
@@ -0,0 +1,13 @@
+package LibraryManagement;
+import java.util.*;
+
+public class Book
+{
+    private int sNo;
+    private String bookName;
+    private String authorName;
+    private float price;
+
+
+
+}
---
+++ LibraryMain.java
@@ -0,0 +1,13 @@
+package LibraryManagement;
+
+import javax.swing.*;
+
+public class LibraryMain {
+    public static void main(String[] args)
+    {
+        Library x=new Library();
+
+    }
+}
+
+
